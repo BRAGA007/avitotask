60967d18a7f87ad6a7f78d7deb4a0f0afaef6b13
_Braga_ <brazhnik1602@mail.ru>
2022-11-17 13:44:34 +0300

    withdraw balance add

--- pkg/users/withdraw_balance.go
+++ pkg/users/withdraw_balance.go
@@ -1,1 +1,41 @@
 package users
+
+import (
+	"avitotask/pkg/common/models"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
+
+type WithDrawBalanceRequestBody struct {
+	Id         uint `json:"user_id"`
+	Withdrawal int  `json:"withdrawal"`
+}
+
+func (h handler) WithDrawBalance(c *gin.Context) {
+
+	body := WithDrawBalanceRequestBody{}
+
+	// получаем тело запроса
+	if err := c.BindJSON(&body); err != nil {
+		c.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+
+	var user models.User
+	var transaction models.Transaction
+
+	if result := h.DB.First(&user, body.Id); result.Error != nil {
+		c.AbortWithError(http.StatusBadRequest, result.Error)
+		c.JSON(http.StatusBadRequest, "У данного пользователя отсутствет баланс")
+		return
+
+	} else {
+		user.Balance -= body.Withdrawal
+		transaction.User_id = body.Id
+		transaction.Desciption = "Вывод средств на сумму: " + strconv.Itoa(body.Withdrawal) + " копеек"
+	}
+	h.DB.Save(&user)
+	h.DB.Save(&transaction)
+	c.JSON(http.StatusOK, &user)
+}
